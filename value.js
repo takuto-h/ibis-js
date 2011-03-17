4948d02fd537c867de305a0cfc0599ad33261619
Takuto Hayashi <takuto.hayashi@gmail.com>
2011-03-17 15:58:56 +0900

    value.js: Add ValueClosure#call

--- value.js
+++ value.js
@@ -3,6 +3,11 @@ function ValueClosure(varName, expr) {
   this.expr = expr;
 }
 ValueClosure.prototype = {
+  call: function (arg, env) {
+    var newEnv = new EnvLocal(env);
+    newEnv.add(this.varName, arg);
+    return this.expr.eval(newEnv);
+  },
   toString: function () {
     return "#<closure>"
   }
