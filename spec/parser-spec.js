285c27b69b8c1c8fac9073c5b1ba1b623215933c
Takuto Hayashi <takuto.hayashi@gmail.com>
2011-03-22 17:18:02 +0900

    Divide parser specs

--- spec/parser-spec.js
+++ spec/parser-spec.js
@@ -1,18 +1,27 @@
 describe("Parser", function() {
   var Parser = Ibis.Parser;
   
-  it("can parse atoms", function() {
+  it("can parse integers", function() {
     var parser = Parser.ofString("123");
     expect(Parser.parse(parser).toString()).toEqual("(Const 123)");
+  });
+  
+  it("can parse variables", function() {
     var parser = Parser.ofString("abc");
     expect(Parser.parse(parser).toString()).toEqual("(Var abc)");
+  });
+  
+  it("can parse function abstractions", function() {
     var parser = Parser.ofString("fun x -> x");
     expect(Parser.parse(parser).toString()).toEqual("(Abs x (Var x))");
+  });
+  
+  it("can parse let expressions", function() {
     var parser = Parser.ofString("let x = 1");
     expect(Parser.parse(parser).toString()).toEqual("(Let x (Const 1))");
   });
   
-  it("can parse primary expressions", function() {
+  it("can parse function applications", function() {
     var parser = Parser.ofString("f x y");
     expect(Parser.parse(parser).toString()).toEqual(
       "(App (App (Var f) (Var x)) (Var y))"
